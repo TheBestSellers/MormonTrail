c2c55a5ad2ab8735fabc2c2b4117b6440572330f
Alyssa Hundley <ahundley11@gmail.com>
2018-02-03 11:55:40 -0700

    2-1-2018 Commit for team assignment with test comments #2

--- MormonTrailProject/src/byui/cit260/mormonTrail/model/Scene.java
+++ MormonTrailProject/src/byui/cit260/mormonTrail/model/Scene.java
@@ -6,6 +6,7 @@
 package byui.cit260.mormonTrail.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -14,4 +15,47 @@ import java.io.Serializable;
 public class Scene implements Serializable{
     
     private String description;
+
+    public Scene() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Scene other = (Scene) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Scene{" + "description=" + description + '}';
+    }
+    
+    
 }
