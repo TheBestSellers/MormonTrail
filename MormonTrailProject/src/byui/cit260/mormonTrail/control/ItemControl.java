3f408d58494746509f8028b04b6e9ef554cbc6c3
Admin <Admin@CSel>
2018-02-17 09:37:05 -0700

    Changed the Integers to ints

--- MormonTrailProject/src/byui/cit260/mormonTrail/control/ItemControl.java
+++ MormonTrailProject/src/byui/cit260/mormonTrail/control/ItemControl.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package byui.cit260.mormonTrail.control;
 
 /**
  *
- * @author alyssahundley
+ * @author ChristopherSellers
  */
 public class ItemControl {
     
+    public static int calcFoodDraw(double stamina, double pace) {
+        
+        int errorCode = 0;
+        
+        if(stamina < 1 || stamina > 5) {
+            errorCode--;
+        }
+        
+        if(pace < 1 || pace > 2) {
+            errorCode -= 2;
+        }
+        
+        if(errorCode < 0) {
+            return errorCode;
+        }
+        
+        int foodDraw = (int)Math.round((((stamina/2)*pace)+2)*2);
+        return foodDraw;
+    }
+    
 }
+
+
+//calcFoodDraw(Stamina: int, Pace: double) returns int
+//
+//If stamina is not between 1 and 5, return -1;
+//If pace is not between 1 and 2, return -2;
+//If both are invalid, return -3;
+//If parameters are all valid, calculate FoodDraw: (((Stamina/2)Pace)+2)*2, round to nearest int and return;
+//
+//Return FoodDraw
---
+++ MormonTrailProject/test/byui/cit260/mormonTrail/control/ItemControlTest.java
@@ -0,0 +1,84 @@
+package byui.cit260.mormonTrail.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Christopher Sellers
+ */
+public class ItemControlTest {
+    
+    public ItemControlTest() {
+    }
+
+    /**
+     * Test of calcFoodDraw method, of class ItemControl.
+     */
+    @Test
+    public void testCalcFoodDraw1() {
+        System.out.println("calcFoodDraw1");
+        int stamina = 3;
+        double pace = 1.5;
+        int expResult = 9;
+        int result = ItemControl.calcFoodDraw(stamina, pace);
+        System.out.println(result + " vs " + expResult);
+        assertEquals(expResult, result);
+        
+    }
+    
+    @Test
+    public void testCalcFoodDraw2 () {
+        System.out.println("calcFoodDraw2");
+        int stamina = 6;
+        double pace = 1.0;
+        int expResult = -1;
+        int result = ItemControl.calcFoodDraw(stamina, pace);
+        System.out.println(result + " vs " + expResult);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testCalcFoodDraw3() {
+        System.out.println("calcFoodDraw3");
+        int stamina = 5;
+        double pace = 2.5;
+        int expResult = -2;
+        int result = ItemControl.calcFoodDraw(stamina, pace);
+        System.out.println(result + " vs " + expResult);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testCalcFoodDraw4() {
+        System.out.println("calcFoodDraw4");
+        int stamina = 0;
+        double pace = 0.5;
+        int expResult = -3;
+        int result = ItemControl.calcFoodDraw(stamina, pace);
+        System.out.println(result + " vs " + expResult);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testCalcFoodDraw5() {
+        System.out.println("calcFoodDraw5");
+        int stamina = 1;
+        double pace = 1.0;
+        int expResult = 5;
+        int result = ItemControl.calcFoodDraw(stamina, pace);
+        System.out.println(result + " vs " + expResult);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testCalcFoodDraw6() {
+        System.out.println("calcFoodDraw6");
+        int stamina = 5;
+        double pace = 2.0;
+        int expResult = 14;
+        int result = ItemControl.calcFoodDraw(stamina, pace);
+        System.out.println(result + " vs " + expResult);
+        assertEquals(expResult, result);   
+    }
+}
