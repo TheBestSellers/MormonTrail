a2199b6665e9912c80e1ec60717d760283a8553b
sariah <saria@SARIAH-PC>
2018-03-09 12:44:54 -0700

    3/9/18 - River Crossing Menu Extends View 	   - riverProbabilityCrossingView stub created

---
+++ MormonTrailProject/src/byui/cit260/mormonTrail/view/riverCrossingMenuView.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.mormonTrail.view;
+
+/**
+ *
+ * @author Sariah
+ */
+public class riverCrossingMenuView extends View {
+
+    private dropRiverSuppliesView dropRiverSuppliesView;
+    private riverCrossingProbabilityView riverCrossingProbabilityView;
+    
+     public riverCrossingMenuView(){
+              super("Welcome to the River Crossing"
+              + "\nPlease select an option"
+              + "\nD - Drop Supplies"
+              + "\nC - Cross the river"
+              + "\nB - Go back and think about it\n");
+    }
+    
+    @Override
+    public boolean doAction(String[] inputs) {
+    String menuItem = inputs[0];
+       
+        switch (menuItem) {
+            case "D": dropRiverSuppliesView();
+                break;
+            case "C": riverCrossingProbabilityView();
+                break;
+            case "B": return true;
+                
+            default: System.out.println("Invalid menu item");
+        }
+        return false;
+        
+    }
+
+    private void dropRiverSuppliesView() {
+        dropRiverSuppliesView = new dropRiverSuppliesView();
+        dropRiverSuppliesView.display();
+    }
+
+    private void riverCrossingProbabilityView() {
+        riverCrossingProbabilityView = new riverCrossingProbabilityView();
+        riverCrossingProbabilityView.display();
+    }
+    
+}
