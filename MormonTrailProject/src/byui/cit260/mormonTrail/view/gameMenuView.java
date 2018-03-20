a02151663a6ac36311937b1525c4579825d6f751
Alyssa Hundley <ahundley11@gmail.com>
2018-03-19 22:39:54 -0600

    Map Display View

--- MormonTrailProject/src/byui/cit260/mormonTrail/view/gameMenuView.java
+++ MormonTrailProject/src/byui/cit260/mormonTrail/view/gameMenuView.java
@@ -6,7 +6,9 @@
 package byui.cit260.mormonTrail.view;
 
 import byui.cit260.mormonTrail.control.GameControl;
+import byui.cit260.mormonTrail.control.MapControl;
 import byui.cit260.mormonTrail.control.PeopleControl;
+import byui.cit260.mormonTrail.model.Item;
 import byui.cit260.mormonTrail.model.Location;
 import mormontrailproject.MormonTrailProject;
 
@@ -50,9 +52,25 @@ public gameMenuView(){
     }
 
     public void displayMap(){
-       for (Location location : MormonTrailProject.getMap().locations) {
-           System.out.println(location);
-       }
+        
+       MapControl mc = new MapControl();
+       Location[] locations =  mc.createLocations();
+       
+       //for (Location location : MormonTrailProject.getMap().locations) {
+           System.out.println("**********************************************************************************************************\n|START|\n\n"
+           +"   Nauvoo     " + locations[1].getName() + "     "+ locations[3].getName() + "     "+locations[5].getName() +"     "+ locations[7].getName() + "     "+locations[9].getName() 
+           +"\n     T            " + locations[1].getSymbol() + "                 " + locations[3].getSymbol() + "                 " + locations[5].getSymbol() + "                 " + locations[7].getSymbol() + "                  "+ locations[9].getSymbol()
+           +"\n     |            |                 |                 |                 |                  |"
+           +"\n     |            |                 |                 |                 |                  |" +
+  
+           "\n==========================================================================================================\n" +
+           "         |                |                   |                |                 |                |\n"
+           + "         |                |                   |                |                 |                |\n"
+           + "         " + locations[0].getSymbol() + "                " + locations[2].getSymbol() + "                   " + locations[4].getSymbol() + "                " + locations[6].getSymbol() + "                 " + locations[8].getSymbol() + "                " + locations[10].getSymbol()
+           + "\n     " + locations[0].getName() + "      " + locations[2].getName() + "          " +locations[4].getName() + "      " +locations[6].getName() + "        " +locations[8].getName() +"       " + locations[10].getName() + 
+           "\n\n                                                                                                     |END|"+
+           "\n**********************************************************************************************************");
+       //}
     }
     
     private void displayCharacters() {
